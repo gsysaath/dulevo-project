4256829f9e7cfd83a987a02890e15705ac067c06
georges sysaath <georges.sysaath@gmail.com>
2021-03-25 11:39:30 +0100

    flatpickr datepickr and form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,18 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import { initFlatpickr } from "../plugins/flatpickr";
+import { initTimepickr } from "../plugins/timepickr";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
+
+document.addEventListener("turbolinks:load", () => {
+  initFlatpickr();
+  initTimepickr();
+});
